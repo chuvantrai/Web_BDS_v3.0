869fd55880b1e34025bab7c37b708cbe2ac6bc58
traicv <traicv@nvoaon.asia>
2023-04-25 18:02:18 +0700

    up

--- Web_BDS/WebBDS/Controllers/HomeController.cs
+++ Web_BDS/WebBDS/Controllers/HomeController.cs
@@ -1,59 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-using Newtonsoft.Json;
-using WebBDS.Models;
-using System.Linq;
 
-namespace WebBDS.Controllers
+namespace WebBDS.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    // GET
+    public IActionResult Index()
     {
-        private readonly ILogger<HomeController> _logger;
-        private readonly Bds_CShapContext _context;
-
-        public HomeController(ILogger<HomeController> logger, Bds_CShapContext context)
-        {
-            _logger = logger;
-            _context = context;
-        }
-
-        public IActionResult Index(string tb)
-        {
-            // lay Acc user
-            var jsonStr = Request.Cookies["useraccount"];
-            User user;
-            if (jsonStr is null) { user = new User(); }
-            else { user = JsonConvert.DeserializeObject<User>(jsonStr); ViewBag.User = user; }
-
-            if (tb!=null&&tb.Equals("1")) { ViewBag.thongbao = "Đã gửi yêu cầu thành công!"; }
-            List<News> listnews = _context.News.OrderByDescending(x=>x.DateUp).Skip(0).Take(3).ToList();
-            ViewBag.listnews= listnews;
-            
-            List<Product> listcanho = _context.Products.OrderByDescending(x=>x.ProductId)
-                .Where(x=>x.Category.CategoryName.Equals("Căn hộ")).Skip(0).Take(3).ToList();
-            ViewBag.listcanho = listcanho;
-            List<Product> listdatnen = _context.Products.OrderByDescending(x => x.ProductId)
-                .Where(x => x.Category.CategoryName.Equals("Đất nền")).Skip(0).Take(3).ToList();
-            ViewBag.listdatnen = listdatnen;
-            List<Product> listnhapho = _context.Products.OrderByDescending(x => x.ProductId)
-                .Where(x => x.Category.CategoryName.Equals("Nhà Phố")).Skip(0).Take(3).ToList();
-            ViewBag.listnhapho = listnhapho;
-            List<Product> listbietthu = _context.Products.OrderByDescending(x => x.ProductId)
-                .Where(x => x.Category.CategoryName.Equals("Biệt Thự")).Skip(0).Take(3).ToList();
-            ViewBag.listbietthu = listbietthu;
-            ViewBag.listregi = _context.Regionals.ToList();
-            return View("Index");
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        return View("Index");
     }
 }
